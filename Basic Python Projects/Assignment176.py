339054ade1dd9dfa10badcd65bd1f3b5a040fb76
mdarlow <66172619+mdarlow@users.noreply.github.com>
2020-07-29 18:52:03 -0700

    Push Assignment176.py .

---
+++ Basic Python Projects/Assignment176.py
@@ -0,0 +1,31 @@
+#       ============
+#       Assignment 176
+#       ============
+#
+#       Python:   3.8.5
+#
+#       Author:   Michael B. Darlow
+#
+#       Purpose:   The Tech Academy -- Python Course. Create two classes that inherit from another class.
+#
+#       Requirements:   -Each child should have at least two of their own attributes.
+#                       
+#
+#
+#
+
+class Fish:
+    name = " "
+    pH_levels = " "
+    food = " "
+    swimming_level = " "
+
+
+class Saltwater_Fish(Fish):
+    current_flow = " "
+    is_reef_safe = " "
+
+class Brackish_Fish(Fish):
+    salinity_level = " "
+    adaptability_level = " "
+    
