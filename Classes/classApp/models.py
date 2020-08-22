fb3b5e70ee6d5c077503b682ce322163c17a57b4
mdarlow <66172619+mdarlow@users.noreply.github.com>
2020-08-21 21:36:43 -0700

    Push Classes/classApp

--- Classes/classApp/models.py
+++ Classes/classApp/models.py
@@ -2,13 +2,12 @@ from django.db import models
 
 
 class djangoClasses(models.Model):
-    type = models.CharField(max_length=60)
     title = models.CharField(max_length=60, default="", blank=True, null=False)
-    course_number = models.IntegerField(max_length=5, default="", blank=True, null=False)
+    course_number = models.IntegerField(default="", blank=True, null=False)
     instructor = models.CharField(max_length=60, default="", blank=True, null=False)
     duration = models.DecimalField(default=0.00, max_digits=10000, decimal_places=2)
 
     objects = models.Manager()
 
     def __str__(self):
-        return self.name
+        return self.title
